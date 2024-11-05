df73f755528f1c5253f03c413d48064c81d87d92
Valentina Tabares <valentinatabares212@gmail.com>
2024-11-05 16:24:36 -0500

    vercel

--- index.js
+++ index.js
@@ -1,104 +1,34 @@
-"use strict";
+const express = require('express');
+const { urlencoded, json } = require('express');
+const pool = require('./database/mongo');
+const router = require('./routes/margarita.routes.js');
+const cors = require('cors');
+require('dotenv').config();
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+
+const app = express();
+
+// Middleware para analizar datos codificados y JSON
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cors());
+
+// Manejador para la ruta raíz
+app.get('/', (req, res) => {
+    res.send('Bienvenido al backend de Kevin!');
 });
-Object.defineProperty(exports, "MAX", {
-  enumerable: true,
-  get: function () {
-    return _max.default;
-  }
+
+// Manejador para la ruta GET que imprime un mensaje
+app.get('/v1/signos', (req, res, next) => {
+    console.log('Estás en el backend de Kevin');
+    next();
 });
-Object.defineProperty(exports, "NIL", {
-  enumerable: true,
-  get: function () {
-    return _nil.default;
-  }
+
+// Usar el router para las rutas relacionadas con 'margarita'
+app.use('/v1/margarita', router);
+
+// Iniciar el servidor
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
-Object.defineProperty(exports, "parse", {
-  enumerable: true,
-  get: function () {
-    return _parse.default;
-  }
-});
-Object.defineProperty(exports, "stringify", {
-  enumerable: true,
-  get: function () {
-    return _stringify.default;
-  }
-});
-Object.defineProperty(exports, "v1", {
-  enumerable: true,
-  get: function () {
-    return _v.default;
-  }
-});
-Object.defineProperty(exports, "v1ToV6", {
-  enumerable: true,
-  get: function () {
-    return _v1ToV.default;
-  }
-});
-Object.defineProperty(exports, "v3", {
-  enumerable: true,
-  get: function () {
-    return _v2.default;
-  }
-});
-Object.defineProperty(exports, "v4", {
-  enumerable: true,
-  get: function () {
-    return _v3.default;
-  }
-});
-Object.defineProperty(exports, "v5", {
-  enumerable: true,
-  get: function () {
-    return _v4.default;
-  }
-});
-Object.defineProperty(exports, "v6", {
-  enumerable: true,
-  get: function () {
-    return _v5.default;
-  }
-});
-Object.defineProperty(exports, "v6ToV1", {
-  enumerable: true,
-  get: function () {
-    return _v6ToV.default;
-  }
-});
-Object.defineProperty(exports, "v7", {
-  enumerable: true,
-  get: function () {
-    return _v6.default;
-  }
-});
-Object.defineProperty(exports, "validate", {
-  enumerable: true,
-  get: function () {
-    return _validate.default;
-  }
-});
-Object.defineProperty(exports, "version", {
-  enumerable: true,
-  get: function () {
-    return _version.default;
-  }
-});
-var _max = _interopRequireDefault(require("./max.js"));
-var _nil = _interopRequireDefault(require("./nil.js"));
-var _parse = _interopRequireDefault(require("./parse.js"));
-var _stringify = _interopRequireDefault(require("./stringify.js"));
-var _v = _interopRequireDefault(require("./v1.js"));
-var _v1ToV = _interopRequireDefault(require("./v1ToV6.js"));
-var _v2 = _interopRequireDefault(require("./v3.js"));
-var _v3 = _interopRequireDefault(require("./v4.js"));
-var _v4 = _interopRequireDefault(require("./v5.js"));
-var _v5 = _interopRequireDefault(require("./v6.js"));
-var _v6ToV = _interopRequireDefault(require("./v6ToV1.js"));
-var _v6 = _interopRequireDefault(require("./v7.js"));
-var _validate = _interopRequireDefault(require("./validate.js"));
-var _version = _interopRequireDefault(require("./version.js"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
